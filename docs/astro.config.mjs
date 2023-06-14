c6e8b014499b281721293b238b8355f57d0aa153
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-06-14 01:38:19 -0600

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
       title: 'Starlight',
       logo: {
         light: '/src/assets/logo-light.svg',
-        dark: '/src/assets/logo-dark.svg',
+        dark: '/src/assets/Copia-de-Encabezado-email-SAPI-unscreen.gif',
         replacesTitle: true,
       },
       editLink: {
