4ca9a0207657633e410902cf0916a6b482c2ee54
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-06-16 21:36:12 -0600

    Update config.ts

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,7 +1,8 @@
-import { defineData } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
-export const data = {
-  docs: defineData({ schema: docsSchema() }),
-  i18n: defineData({ schema: i18nSchema() }),
+export const collections = {
+docs: defineCollection({ schema: docsSchema() }),
+i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
+
